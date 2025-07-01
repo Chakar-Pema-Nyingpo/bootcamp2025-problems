cff05c4f27890418035eea45bd02d3bad8343c7a
Chakar Pema Nyingpo <chakarpemanyingpo@Chakars-MacBook-Air.local>
2025-07-01 12:44:42 +0600

    codewar problems

--- function.js
+++ function.js
@@ -4,14 +4,14 @@ function reverseString(str) {
 
 function replace(str, charToReplace, strReplacement) {
   const result = str.replaceAll(charToReplace, strReplacement);
-  console.log(result);
+  // console.log(result);
   return result;
 }
 replace("hello", "l", "p");
 
 function strLengthEven(str) {
   const result = str.length % 2 === 0;
-  console.log(result);
+  // console.log(result);
   return result;
 }
 strLengthEven("Hello");
@@ -22,4 +22,21 @@ function findRabbit(arr) {
 
 function subtractTen(num) {
   return num - 10;
+}
+
+//Changing century from year
+function century(year) {
+  return Math.ceil(year / 100);
+}
+// console.log(century(1996));
+
+//Checking the end from the string
+function solution(str, ending) {
+  return str.endsWith(ending);
+}
+// console.log(solution("hello everybody", "ody"));
+
+// https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 !== flower2 % 2;
 }
