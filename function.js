45de64e95e20e837a710bb99fc9e9ac199f5b8f7
Chakar Pema Nyingpo <chakarpemanyingpo@Chakars-MacBook-Air.local>
2025-07-01 09:01:01 +0600

    Updates on new functions

--- function.js
+++ function.js
@@ -2,8 +2,20 @@ function reverseString(str) {
   return str.split("").reverse().join();
 }
 
-function replace(str, charToReplace, strReplacement) {}
+function replace(str, charToReplace, strReplacement) {
+  const result = str.replaceAll(charToReplace, strReplacement);
+  console.log(result);
+  return result;
+}
+replace("hello", "l", "p");
 
-function strLengthEven(str) {}
+function strLengthEven(str) {
+  const result = str.length % 2 === 0;
+  console.log(result);
+  return result;
+}
+strLengthEven("Hello");
 
-function findRabbit() {}
+function findRabbit(arr) {
+  return arr.map((word) => word.toLowerCase()).indexOf("rabbit");
+}
