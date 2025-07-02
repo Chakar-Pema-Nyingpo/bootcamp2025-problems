efa5439a1aa674a56913a43460922938f25429c2
Chakar Pema Nyingpo <chakarpemanyingpo@Chakars-MacBook-Air.local>
2025-07-02 10:58:05 +0600

    adds function from codewar

--- function.js
+++ function.js
@@ -58,3 +58,10 @@ function getSum(a, b) {
   }
   return sum;
 }
+
+// https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/javascript
+function longest(s1, s2) {
+  return Array.from(new Set(s1 + s2))
+    .sort()
+    .join("");
+}
